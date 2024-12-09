f4fdc2d5b13545eb4b98a4625154d34436059149
Noah G. Singer <ngsinger@cs.cmu.edu>
2024-12-09 13:57:42 -0500

    starting to implement alpha+beta+gamma

--- Steinberg/Defs/Basic.lean
+++ Steinberg/Defs/Basic.lean
@@ -2,9 +2,9 @@ import Mathlib.Algebra.Group.Commutator
 import Mathlib.Tactic.Group
 
 variable {G : Type Tu} [Group G]
+         {R : Type Tv} [Ring R]
 
 /- commutator identities (holding in any group) -/
-
 theorem comm_left_str  (x y : G)   : x * y = ⁅x, y⁆ * y * x := by group
 theorem comm_right_str (x y : G)  : x * y = y * x * ⁅x⁻¹, y⁻¹⁆ := by group
 
@@ -16,3 +16,61 @@ theorem comm_on_left {x y z : G} : x * y = z * y * x → ⁅x, y⁆ = z := by
   group
   rw [h]
   group
+
+/- generic forms for propositions -/
+/- group theory -/
+@[reducible]
+-- `x * y = y * x`
+def CommutesProp (x y : G) : Prop :=
+  x * y = y * x
+
+@[reducible]
+-- `x * y = z * y * x`
+def ReorderLeftProp (x y z : G) : Prop :=
+  x * y = z * y * x
+
+@[reducible]
+def TrivialCommutatorProp (x y : G) : Prop :=
+  ⁅ x, y ⁆ = 1
+
+@[reducible]
+def CommutatorProp (x y z : G) : Prop :=
+  ⁅ x, y ⁆ = z
+
+/- groups and rings -/
+@[simp]
+-- `(f t) * (f u) = f (t + u)`
+def LinearityProp (f : R → G) : Prop :=
+  ∀ (t u : R), (f t) * (f u) = f (t + u)
+
+-- `f(0) = 0`
+@[simp]
+def IdentityProp (f : R → G) : Prop :=
+  f (0 : R) = (1 : G)
+
+-- `f(t)⁻¹ = f(-t)`
+@[simp]
+def InverseProp (f : R → G) : Prop :=
+  ∀ (t : R), f (-t) = (f t)⁻¹
+
+-- deduce an IdentityProp from an LinearityProp
+theorem IdentityFromLinearity (f : R → G) : LinearityProp f → IdentityProp f := by
+  intro h
+  rw [IdentityProp]
+  apply @mul_left_cancel _ _ _ (f 0)
+  rw [mul_one]
+  rw [h]
+  rw [add_zero]
+  done
+
+-- deduce an InverseProp from a LinearityProp
+theorem InverseFromLinearity (f : R → G) : LinearityProp f → InverseProp f := by
+  intro h
+  rw [InverseProp]
+  intro t
+  apply @mul_left_cancel _ _ _ (f t)
+  rw [h]
+  rw [add_neg_cancel t]
+  rw [IdentityFromLinearity f h]
+  rw [mul_inv_cancel]
+  done
--- Steinberg/Defs/LiftA3.lean
+++ Steinberg/Defs/LiftA3.lean
@@ -44,7 +44,7 @@ abbrev Deg (n : ℕ) := Fin (n + 1)
 
 /- defining the A3 positive root system -/
 inductive A3PositiveRoot
-  | α | β | γ | αβ | βγ
+  | α | β | γ | αβ | βγ | αβγ
 
 namespace A3PositiveRoot
 
@@ -52,48 +52,38 @@ namespace A3PositiveRoot
 --     so that you don't have to manually unfold it during proving.
 @[reducible]
 def height : A3PositiveRoot → Nat
-  | A3PositiveRoot.α => 1
-  | A3PositiveRoot.β => 1
-  | A3PositiveRoot.γ => 1
-  | A3PositiveRoot.αβ => 2
-  | A3PositiveRoot.βγ => 2
+  | α => 1
+  | β => 1
+  | γ => 1
+  | αβ => 2
+  | βγ => 2
+  | αβγ => 3
 
 def toString : A3PositiveRoot → String
   | α => "α"
   | β => "β"
   | γ => "γ"
-  | αβ => "αβ"
-  | βγ => "βγ"
+  | αβ => "α+β"
+  | βγ => "β+γ"
+  | αβγ => "α+β+γ"
+
+@[simp]
+def isPresent : A3PositiveRoot → Bool
+  | αβγ => false
+  | _ => true
 
 instance instToString : ToString A3PositiveRoot := ⟨toString⟩
 
 end A3PositiveRoot
 
-
 structure A3UnipGen (R : Type Tv) [Ring R] where
-  root : A3PositiveRoot
-  coeff : R
-  i : Deg root.height
+  ζ : A3PositiveRoot -- really, this should only be allowed to be present...
+  i : Deg ζ.height
+  t : R
 
 namespace A3UnipGen
 
 open A3PositiveRoot
-
-def mkOf (r : A3PositiveRoot) (coeff : R) (i : Deg r.height) :=
-  FreeGroup.of <| mk r coeff i
-
-/-- Shorthand for building free group elements from a root, degree, and ring element. -/
-scoped notation "{" r ", " coeff ", " i "}" => A3UnipGen.mkOf r coeff i
-
-/--
-  Additional shorthand for building free group elements from a root, degree, and ring element.
-
-  CC: It turns out that both `{ }` and `( )` are "reserved syntax" by Lean, and so
-      it can't make up its mind if you use things that are also reserved for
-      (Fin)sets and Prods. Thus, if you get an "ambiguous, possible interpretations"
-      error, you can fall back on using the pipe character '|' to wrap the triple.
--/
-scoped notation "|" r ", " coeff ", " i "|" => A3UnipGen.mkOf r coeff i
 
 /--
   Syntax for adding two `Deg`s together, and then automatically deriving the
@@ -109,16 +99,45 @@ syntax (name := degAdd) term " +' " term : term
 macro_rules
   | `($x +' $y) => `(⟨($x).val + ($y).val, by first | trivial | omega | simp [height] at *; omega⟩)
 
-def Linearity (R : Type Tv) [Ring R] : Prop :=
-  ∀ (r : A3PositiveRoot) (t u : R) (i : Deg r.height),
-    {r, t, i} * {r, u, i} = {r, t + u, i}
+-- "{αβγ t i}" -> ⁅ |α, i, t1|, |βγ, (1 : R), i2| ⁆
 
--- nontrivial commutators
-def α_comm_β (R : Type Tv) [Ring R] := ∀ (t u : R) (i : Deg α.height) (j : Deg β.height),
- ⁅ {α, t, i}, {β, u, j} ⁆ = |αβ, (t * u), (i +' j)|
+def split_deg3 (i : Deg 3) : (Deg 1) × (Deg 2) :=
+  match i with
+  | 0 => (0, 0)
+  | 1 => (0, 1)
+  | 2 => (0, 2)
+  | 3 => (1, 2)
 
-def β_comm_γ (R : Type Tv) [Ring R] := ∀ (t u : R) (i : Deg β.height) (j : Deg γ.height),
- ⁅ {β, t, i}, {γ, u, j} ⁆ = |βγ, (t * u), (i +' j)|
+-- theorem split_deg3_sum (i : Deg 3) :
+--   let (i₁, i₂) := split_deg3 i
+--   i.val = i₁.val + i₂.val := by
+--   match i with
+--   | 0 => omega
+--   | 1 => omega
+--   | 2 => omega
+--   | 3 => omega
+--   done
+
+def mkOf (ζ : A3PositiveRoot) (i : Deg ζ.height) (t : R) : FreeGroup (A3UnipGen R) :=
+  match ζ with
+    | αβγ =>
+      let (i₁, i₂) := split_deg3 i
+      ⁅ mkOf α i₁ t, mkOf βγ i₂ (1 : R) ⁆
+    | ζ => FreeGroup.of <| mk ζ i t
+termination_by ζ.height
+
+/-- Shorthand for building free group elements from a root, degree, and ring element. -/
+scoped notation "{" ζ ", " i ", " t "}" => A3UnipGen.mkOf ζ i t
+
+/--
+  Additional shorthand for building free group elements from a root, degree, and ring element.
+
+  CC: It turns out that both `{ }` and `( )` are "reserved syntax" by Lean, and so
+      it can't make up its mind if you use things that are also reserved for
+      (Fin)sets and Prods. Thus, if you get an "ambiguous, possible interpretations"
+      error, you can fall back on using the pipe character '|' to wrap the triple.
+-/
+scoped notation "|" ζ ", " i ", " t "|" => A3UnipGen.mkOf ζ i t
 
 /-
 open Lean in
@@ -128,23 +147,45 @@ macro "declare_trivial_commutator" rootOne:ident rootTwo:ident : command => do
   let mut cmds ← Syntax.getArgs <$> `(
     def $thmName (R : Type Tv) [Ring R] :=
       ∀ (t u : R) (i : Deg $rootOne.height) (j : Deg $rootTwo.height),
-        ⁅ { $rootOne, t, i }, { $rootTwo, u, j } ⁆ = 1
+        ⁅ { $rootOne, i, t }, { $rootTwo, j, u } ⁆ = 1
   )
   return (mkNullNode cmds) -/
 
+-- types of statements which we assume or want to prove about roots...
+abbrev trivial_commutator_of_root_pair (ζ η : A3PositiveRoot) (R : Type Tv) [Ring R] : Prop :=
+  ∀ (t u : R) (i : Deg ζ.height) (j : Deg η.height),
+    TrivialCommutatorProp {ζ, i, t} {η, j, u}
+
+abbrev linearity_of_root (ζ : A3PositiveRoot) (R : Type Tv) [Ring R] : Prop :=
+  ∀ (t u : R) (i : Deg ζ.height), {ζ, i, t} * {ζ, i, u} = {ζ, i, t+u}
+
+abbrev id_of_root (ζ : A3PositiveRoot) (R : Type Tv) [Ring R] : Prop :=
+  ∀ (i : Deg ζ.height), {ζ, i, (0 : R)} = 1
+
+abbrev inv_of_root (ζ : A3PositiveRoot) (R : Type Tv) [Ring R] : Prop :=
+  ∀ (t : R) (i : Deg ζ.height), {ζ, i, -t} = {ζ, i, t}⁻¹
+
+-- assumptions
 -- trivial commutators
+def β_comm_αβ  := trivial_commutator_of_root_pair β αβ
+def γ_comm_βγ  := trivial_commutator_of_root_pair γ βγ
+def α_comm_γ   := trivial_commutator_of_root_pair α γ
+def αβ_comm_βγ := trivial_commutator_of_root_pair αβ βγ
 
-def trivial_commutator (r₁ r₂ : A3PositiveRoot) (R : Type Tv) [Ring R] : Prop :=
-  ∀ (t u : R) (i : Deg r₁.height) (j : Deg r₂.height),
-    ⁅ {r₁, t, i}, {r₂, u, j} ⁆ = 1
+-- linearity
+def lin_of_present (R : Type Tv) [Ring R] : Prop := ∀ (ζ : A3PositiveRoot) (i : Deg ζ.height),
+  -- ζ.isPresent →
+  linearity_of_root ζ R
 
-def β_comm_αβ  := trivial_commutator β αβ
-def γ_comm_βγ  := trivial_commutator γ βγ
-def α_comm_γ   := trivial_commutator α γ
-def αβ_comm_βγ := trivial_commutator αβ βγ
+-- nontrivial commutators
+def α_comm_β (R : Type Tv) [Ring R] := ∀ (t u : R) (i : Deg α.height) (j : Deg β.height),
+ ⁅ {α, i, t}, {β, j, u} ⁆ = |αβ, (i +' j), (t * u)|
+
+def β_comm_γ (R : Type Tv) [Ring R] := ∀ (t u : R) (i : Deg β.height) (j : Deg γ.height),
+ ⁅ {β, i, t}, {γ, j, u} ⁆ = |βγ, (i +' j), (t * u)|
 
 structure WeakA3 (R : Type Tv) [Ring R] where
-  h_lin : Linearity R
+  h_lin : lin_of_present R
   h_α_β : α_comm_β R
   h_β_γ : β_comm_γ R
   h_α_γ : α_comm_γ R
@@ -155,161 +196,119 @@ structure WeakA3 (R : Type Tv) [Ring R] where
 /- analysis of the group -/
 -- deduce identity relations from linearity relations
 @[simp]
-theorem Identity (h : WeakA3 R) (r : A3PositiveRoot) (i : Deg r.height) :
-    {r, (0 : R), i} = 1 := by
-  apply @mul_left_cancel _ _ _ |r, 0, i|
-  rw [mul_one, h.h_lin r (0 : R) (0 : R) i, add_zero]
+theorem id_of_present (h : WeakA3 R) (ζ : A3PositiveRoot) (i : Deg ζ.height) :
+    ζ.isPresent →
+    id_of_root ζ R := by
+  sorry
 
 -- deduce inverse relations from linearity relations
 @[simp]
-theorem Inverse (h : WeakA3 R) (r : A3PositiveRoot) (t : R) (i : Deg r.height) :
-    |r, -t, i| = |r, t, i|⁻¹ := by
-  apply @mul_left_cancel _ _ _ |r, t, i|
-  rw [h.h_lin, add_neg_cancel, Identity h, mul_inv_cancel]
+theorem inv_of_present (h : WeakA3 R) (ζ : A3PositiveRoot) (i : Deg ζ.height) :
+    ζ.isPresent →
+    inv_of_root ζ R := by
+  sorry
 
 -- explicit expressions of commutators
 @[simp]
 theorem expr_βγ_as_β_comm_γ (h : WeakA3 R) (t u : R) (i : Deg β.height) (j : Deg γ.height) :
-    |βγ, (t*u), (i +' j)| = {β, t, i} * {γ, u, j} * {β, (-t), i} * {γ, (-u), j} := by
-  rw [Inverse h β, Inverse h γ, ← commutatorElement_def, ← h.h_β_γ]
+    |βγ, (i +' j), (t*u)| = {β, i, t} * {γ, j, u} * {β, i, (-t)} * {γ, j, (-u)} := by
+  -- have : β.isPresent := rfl
+  have := inv_of_present h i --this
+  simp [this]
+  -- have : γ.isPresent := rfl
+  have := inv_of_present h j --this
+  simp [this]
+  rw [← commutatorElement_def]
+  rw [← h.h_β_γ t u i j]
+  done
 
 -- rewrites for products of noncommuting elements
 @[simp]
 theorem expr_α_β_as_αβ_β_α (h : WeakA3 R) (t u : R) (i : Deg α.height) (j : Deg β.height) :
-    |α, t, i| * {β, u, j} = |αβ, (t*u), (i +' j)| * {β, u, j} * {α, t, i} := by
-  rw [comm_left_str, h.h_α_β]
+    ReorderLeftProp {α, i, t} {β, j, u} |αβ, (i +' j), (t*u)| := by
+  rw [← h.h_α_β]
+  rw [ReorderLeftProp]
+  rw [comm_left_str]
+  done
 
 @[simp]
 theorem expr_β_γ_as_βγ_γ_β (h : WeakA3 R) (t u : R) (i : Deg β.height) (j : Deg γ.height) :
-    |β, t, i| * |γ, u, j| = |βγ, (t * u), (i +' j)| * |γ, u, j| * |β, t, i| := by
-  rw [comm_left_str, h.h_β_γ]
+    ReorderLeftProp {β, i, t} {γ, j, u} |βγ, (i +' j), (t*u)| := by
+  rw [← h.h_β_γ]
+  rw [ReorderLeftProp]
+  rw [comm_left_str]
+  done
 
 -- rewrites for products of commuting elements
 @[simp]
 theorem expr_α_γ_as_γ_α (h : WeakA3 R) (t u : R) (i : Deg α.height) (j : Deg γ.height) :
-    |α, t, i| * |γ, u, j| = |γ, u, j| * |α, t, i| := by
+    CommutesProp {α, i, t} {γ, j, u} := by
   apply comm_to_comm
   rw [h.h_α_γ]
 
 @[simp]
 theorem expr_β_αβ_as_αβ_β (h : WeakA3 R) (t u : R) (i : Deg β.height) (j : Deg αβ.height) :
-    |β, t, i| * |αβ, u, j| = |αβ, u, j| * |β, t, i| := by
+    CommutesProp {β, i, t} {αβ, j, u} := by
   apply comm_to_comm
   rw [h.h_β_αβ]
 
 @[simp]
 theorem expr_γ_βγ_as_βγ_γ (h : WeakA3 R) (t u : R) (i : Deg γ.height) (j : Deg βγ.height) :
-    |γ, t, i| * |βγ, u, j| = |βγ, u, j| * |γ, t, i| := by
+    CommutesProp {γ, i, t} {βγ, j, u} := by
   apply comm_to_comm
   rw [h.h_γ_βγ]
 
 @[simp]
 theorem expr_αβ_βγ_as_βγ_αβ (h : WeakA3 R) (t u : R) (i : Deg αβ.height) (j : Deg βγ.height) :
-    |αβ, t, i| * |βγ, u, j| = |βγ, u, j| * |αβ, t, i| := by
+    CommutesProp {αβ, i, t} {βγ, j, u} := by
   apply comm_to_comm
   rw [h.h_αβ_βγ]
 
 -- interchange theorem, ⁅α, βγ⁆ = ⁅αβ, γ⁆
 theorem Interchange (h : WeakA3 R) (t u v : R) (i : Deg α.height) (j : Deg β.height) (k : Deg γ.height) :
-    ⁅ |α, t, i|, |βγ, (u * v), (j +' k)| ⁆ = ⁅ |αβ, (t * u), (i +' j)|, |γ, v, k| ⁆ := by
+    ⁅ |α, i, t|, |βγ, (j +' k), (u * v)| ⁆ = ⁅ |αβ, (i +' j), (t * u)|, |γ, k, v| ⁆ := by
   apply comm_on_left
   -- phase I: push α to right
-  rw [expr_βγ_as_β_comm_γ h]
-  mul_assoc_l
-  rw [expr_α_β_as_αβ_β_α h]
-  rw [mul_assoc _ |α, t, i|]
-  rw [expr_α_γ_as_γ_α h]
-  mul_assoc_l
-  rw [mul_assoc _ |α, t, i|]
-  rw [expr_α_β_as_αβ_β_α h]
-  rw [mul_neg] -- rewrite t*(-u) as -(t*u)
-  mul_assoc_l
-  rw [mul_assoc _ |α, t, i|]
-  rw [expr_α_γ_as_γ_α h]
-  mul_assoc_l
-  -- phase II: move β's together
-  rw [mul_assoc _ |β, u, j|]
-  rw [expr_β_γ_as_βγ_γ_β h]
-  mul_assoc_l
-  rw [mul_assoc _ |β, u, j|]
-  rw [expr_β_αβ_as_αβ_β h]
-  mul_assoc_l
-  rw [mul_assoc _ |β, u, j|]
-  rw [Inverse h β]
+  conv =>
+    lhs
+    rw [expr_βγ_as_β_comm_γ h]
+    simp [← mul_assoc]
+    rw [expr_α_β_as_αβ_β_α h]
+    rw [mul_assoc _ |α, i, t|]
+    rw [expr_α_γ_as_γ_α h]
+    simp [← mul_assoc]
+    rw [mul_assoc _ |α, i, t|]
+    rw [expr_α_β_as_αβ_β_α h]
+    rw [mul_neg] -- rewrite t*(-u) as -(t*u)
+    simp [← mul_assoc]
+    rw [mul_assoc _ |α, i, t|]
+    rw [expr_α_γ_as_γ_α h]
+    simp [← mul_assoc]
+    -- phase II: move β's together
+    rw [mul_assoc _ |β, j, u|]
+    rw [expr_β_γ_as_βγ_γ_β h]
+    simp [← mul_assoc]
+    rw [mul_assoc _ |β, j, u|]
+    rw [expr_β_αβ_as_αβ_β h]
+    simp [← mul_assoc]
+    rw [mul_assoc _ |β, j, u|]
+  rw [inv_of_present h β i]
   group
-  -- phase III: push βγ to the right
-  rw [mul_assoc _ |βγ, (u * v), (j +' k)|]
-  rw [← expr_γ_βγ_as_βγ_γ h]
-  mul_assoc_l
-  rw [mul_assoc _ |βγ, u * v, j +' k|]
-  rw [← expr_αβ_βγ_as_βγ_αβ h]
-  mul_assoc_l
-  rw [mul_assoc _ |βγ, u * v, j +' k|]
-  rw [← expr_γ_βγ_as_βγ_γ h]
-  mul_assoc_l
-  repeat rw [Inverse h]
-  simp only [← commutatorElement_def]
-  sorry
+  conv =>
+    lhs
+    -- phase III: push βγ to the right
+    rw [mul_assoc _ |βγ, (j +' k), (u * v)|]
+    rw [← expr_γ_βγ_as_βγ_γ h]
+    simp [← mul_assoc]
+    rw [mul_assoc _ |βγ, (j +' k), u * v|]
+    rw [← expr_αβ_βγ_as_βγ_αβ h]
+    simp [← mul_assoc]
+    rw [mul_assoc _ |βγ, (j +' k), u * v|]
+    rw [← expr_γ_βγ_as_βγ_γ h]
+    simp [← mul_assoc]
+  repeat rw [inv_of_present h αβ (i +' j)]
+  repeat rw [inv_of_present h γ k]
+  group
   done
-
-theorem InterchangeEmpty (h : WeakA3 R) (t v : R) (i : Deg α.height) (j : Deg β.height) (k : Deg γ.height) :
-    ⁅ |α, t, i|, |βγ, v, j +' k| ⁆ = ⁅ |αβ, t, i +' j|, |γ, v, k| ⁆ := by
-  nth_rewrite 1 [← one_mul v]
-  nth_rewrite 2 [← mul_one t]
-  rw [Interchange h]
-
-def mkαβγ {R : Type Tv} [Ring R] (t : R) (i : Deg 3) :=
-match i with
-  | 0 => ⁅ |α, t, (0 : Deg 1)|, |βγ, (1 : R), (0 : Deg 2)| ⁆
-  | 1 => ⁅ |α, t, (0 : Deg 1)|, |βγ, (1 : R), (1 : Deg 2)| ⁆
-  | 2 => ⁅ |α, t, (0 : Deg 1)|, |βγ, (1 : R), (2 : Deg 2)| ⁆
-  | 3 => ⁅ |α, t, (1 : Deg 1)|, |βγ, (1 : R), (2 : Deg 2)| ⁆
-
-theorem LinkOne (f : R → R → G) (g : R → R → G) :
-  (∀ (t u v : R), f (t * u) v = g t (u * v)) → (∀ (t u : R), f t u = f (t * u) 1 ∧ g t u = f (t * u) 1) := by
-  intro h
-  intro t u
-  nth_rewrite 1 [← mul_one t]
-  rw [h]
-  rw [one_mul]
-  nth_rewrite 1 [← mul_one u]
-  rw [h]
-  rw [mul_one]
-  simp
-  done
-
--- /- the whopper........ -/
-theorem comm_α_βγ_0 (h : WeakA3 R) :
-  ∀ (t u : R), ⁅ |α, t * u, (0 : Deg 1)|, |βγ, 1, (0 : Deg 2)| ⁆ = ⁅ |α, t, (0 : Deg 1)|, |βγ, u, (0 : Deg 2)| ⁆ := by
-  let f := fun (t : R) (u : R) => ⁅ |α, t * u, (0 : Deg 1)|, |βγ, (1 : R), (0 : Deg 2)| ⁆
-  let g := fun (t : R) (u : R) => ⁅ |α, t, (0 : Deg 1)|, |βγ, u, (0 : Deg 2)| ⁆
-  intro t u
-  rw [f t u]
-  rw [g t u]
-
-
-  rw [mkαβγ]
-
-  have id : ((0 : Deg 1) +' (0 : Deg 1)) = (0 : Deg 2) := by
-    simp
-  intro t u
-  rw [← id]
-  rw [LinkOne]
-  done
-
--- theorem
-
---   ⁅@mkOf _ _ α t 0 (by simp [height] at *), @mkOf _ _ βγ u 0 (by simp [height] at *)⁆ =
---   @mkαβγ _ _ (t * u) 0 := by
---   -- rw [mkαβγ (t*u) 0]
---   nth_rewrite 1 [← one_mul u]
---   rw [Interchange h t 1 u]
---   sorry
-
-
--- theorem comm_α_βγ [Ring R] (t u : R) {i j : Nat} (hi : i ≤ α.height) (hj : j ≤ βγ.height) :
---   ⁅mkOf α t hi, mkOf βγ u hj⁆ = @mkαβγ _ _ (t * u) (i+j) (by simp [height] at *; omega) := by
---   simp_rw [Interchange, mul_one]
---   sorry
 
 end A3UnipGen
