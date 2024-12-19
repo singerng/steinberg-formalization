a1cf9cfacd2e91cd200af22a0720f7bbffbf562d
Noah G. Singer <ngsinger@cs.cmu.edu>
2024-12-19 11:32:50 -0500

    refactored code typechecks (thanks Cayden!)

--- Steinberg/Defs/Root.lean
+++ Steinberg/Defs/Root.lean
@@ -9,30 +9,27 @@ class PosRootSys (Root : Type u) extends ToString Root where
   isPresent : Root → Bool
   add (r₁ r₂ : Root) : Option Root
   mul : PNat → Root → Option Root
-  maker (r : Root) (i : Nat) : Root × Nat × Root × Nat
+  maker (ζ : Root) (i : Nat) : Root × Nat × Root × Nat
 
   -- well-behavedness conditions
-  h_add {r₁ r₂ r₃ : Root} :
-    add r₁ r₂ = some r₃ → height r₃ = height r₁ + height r₂
+  h_add {ζ η θ : Root} :
+    add ζ η = some θ → height θ = height ζ + height η
 
-  h_mul {c : PNat} {r r' : Root} :
-    mul c r = some r' → height r' = c * height r
+  h_mul {c : PNat} {ζ ζ' : Root} :
+    mul c ζ = some ζ' → height ζ' = c * height ζ
 
-  h_maker {r : Root} (hr : ¬isPresent r) {i : Nat} (hi : i ≤ height r) :
-    isPresent (maker r i).1
-    ∧ isPresent (maker r i).2.2.1
-    ∧ (maker r i).2.1 ≤ height (maker r i).1
-    ∧ (maker r i).2.2.2 ≤ height (maker r i).2.2.1
-    ∧ (maker r i).2.1 + (maker r i).2.2.2 = i
-    ∧ add (maker r i).1 (maker r i).2.2.1 = r
-    /-match maker r i with
-    | (r₁, i₁, r₂, i₂) =>
-      isPresent r₁ ∧ isPresent r₂ ∧ i₁ ≤ height r₁ ∧ i₂ ≤ height r₂ ∧ i₁ + i₂ = i ∧ add r₁ r₂ = r -/
+  h_maker {ζ : Root} (hr : ¬isPresent ζ) {i : Nat} (hi : i ≤ height ζ) :
+    isPresent (maker ζ i).1
+    ∧ isPresent (maker ζ i).2.2.1
+    ∧ (maker ζ i).2.1 ≤ height (maker ζ i).1
+    ∧ (maker ζ i).2.2.2 ≤ height (maker ζ i).2.2.1
+    ∧ (maker ζ i).2.1 + (maker ζ i).2.2.2 = i
+    ∧ add (maker ζ i).1 (maker ζ i).2.2.1 = ζ
 
 /-- Applies the `add` operation for a `PosRootSys`. -/
-notation r₁ " +? " r₂ => PosRootSys.add r₁ r₂
+notation ζ₁ " +r " ζ₂ => PosRootSys.add ζ₁ ζ₂
 
 /-- Applies the `mul` operation for a `PosRootSys`. -/
-notation r₁ " *? " r₂ => PosRootSys.mul r₁ r₂
+notation c " *r " ζ => PosRootSys.mul c ζ
 
 end Steinberg
