051e78f64af9fd9e7328a8177491cf5a17c41ad4
Noah G. Singer <ngsinger@cs.cmu.edu>
2024-12-21 14:58:09 -0500

    almost done symmetry proof! (still WIP)

---
+++ Steinberg/Upstream/FreeGroup.lean
@@ -0,0 +1,11 @@
+import Mathlib.GroupTheory.FreeGroup.Basic
+
+theorem lift.hom (α β G : Type u) [Group G] (f : α → FreeGroup β) (g : FreeGroup β →* G) :
+  FreeGroup.lift (g ∘ f) = g.comp (FreeGroup.lift f) := by
+  ext a
+  simp
+
+theorem lift.hom2 (α β : Type u) (f : α → β) :
+  FreeGroup.lift (FreeGroup.of ∘ f) = FreeGroup.map f := by
+  ext a -- only have to prove for generators!
+  simp only [FreeGroup.lift.of, Function.comp_apply, FreeGroup.map.of]
