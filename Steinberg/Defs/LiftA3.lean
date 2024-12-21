cf95f5850821df9c9bd8e231e141b78a51dbb9f4
Cayden Codel <ccodel@andrew.cmu.edu>
2024-12-21 02:49:08 -0500

    feat: shorter proofs with grw; some grw automation

--- Steinberg/Defs/LiftA3.lean
+++ Steinberg/Defs/LiftA3.lean
@@ -399,6 +399,7 @@ theorem expand_βγ_as_β_γ_β_γ :
   rw [← comm_of_β_γ]
 
 /- Rewrite α⬝β as αβ⬝β⬝α. -/
+@[group_reassoc]
 theorem expr_α_β_as_αβ_β_α :
     ∀ {i j : ℕ} (hi : i ≤ α.height) (hj : j ≤ β.height) (t u : R),
       reorder_left({α, i, t}, {β, j, u}, {αβ, (i + j), (t*u)}) := by
@@ -408,6 +409,7 @@ theorem expr_α_β_as_αβ_β_α :
   rw [comm_left]
 
 /- Rewrite β⬝γ as βγ⬝γ⬝β. -/
+@[group_reassoc]
 theorem expr_β_γ_as_βγ_γ_β :
     ∀ {i j : ℕ} (hi : i ≤ β.height) (hj : j ≤ γ.height) (t u : R), reorder_left({β, i, t}, {γ, j, u}, {βγ, (i + j), (t*u)}) := by
   intro i j hi hj t u
@@ -416,6 +418,7 @@ theorem expr_β_γ_as_βγ_γ_β :
   rw [comm_left]
 
 /- Rewrite β⬝γ as γ⬝βγ⬝β. -/
+@[group_reassoc]
 theorem expr_β_γ_as_γ_βγ_β :
   ∀ {i j : ℕ} (hi : i ≤ β.height) (hj : j ≤ γ.height) (t u : R), reorder_mid({β, i, t}, {γ, j, u}, {βγ, (i + j), (t*u)}) := by
   intro i j hi hj t u
@@ -479,46 +482,22 @@ theorem Interchange {i j k : ℕ} (hi : i ≤ α.height) (hj : j ≤ β.height) 
   have hij : i + j ≤ αβ.height := by simp only [height] at *; omega
   have hjk : j + k ≤ βγ.height := by simp only [height] at *; omega
   -- phase I: push α to right
-  conv =>
-    lhs
-    rw [expand_βγ_as_β_γ_β_γ hj hk]
-    simp only [← mul_assoc]
-    rw [expr_α_β_as_αβ_β_α hi hj]
-    rw [mul_assoc _ {α, i, t}]
-    rw [expr_α_γ_as_γ_α hi hk]
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {α, i, t}]
-    rw [expr_α_β_as_αβ_β_α hi hj]
-    rw [mul_neg] -- rewrite t*(-u) as -(t*u)
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {α, i, t}]
-    rw [expr_α_γ_as_γ_α hi hk]
-    simp only [← mul_assoc]
-    -- phase II: move β's together
-    rw [mul_assoc _ {β, j, u}]
-    rw [expr_β_γ_as_βγ_γ_β hj hk]
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {β, j, u}]
-    rw [expr_β_αβ_as_αβ_β hj hij]
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {β, j, u}]
-  rw [inv_of_β]
+  grw [expand_βγ_as_β_γ_β_γ hj hk,
+    expr_α_β_as_αβ_β_α hi hj,
+    expr_α_γ_as_γ_α hi hk,
+    expr_α_β_as_αβ_β_α hi hj,
+    mul_neg,
+    expr_α_γ_as_γ_α hi hk,
+    expr_β_γ_as_βγ_γ_β hj hk,
+    expr_β_αβ_as_αβ_β hj hij,
+    inv_of_β,
+    ← expr_γ_βγ_as_βγ_γ hk hjk,
+    ← expr_αβ_βγ_as_βγ_αβ hij hjk]
   group
-  conv =>
-    lhs
-    -- phase III: push βγ to the right
-    rw [mul_assoc _ {βγ, (j + k), u * v}]
-    rw [← expr_γ_βγ_as_βγ_γ hk hjk]
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {βγ, (j + k), u * v}]
-    rw [← expr_αβ_βγ_as_βγ_αβ hij hjk]
-    simp only [← mul_assoc]
-    rw [mul_assoc _ {βγ, (j + k), u * v}]
-    rw [← expr_γ_βγ_as_βγ_γ hk hjk]
-    simp only [← mul_assoc]
-    rw [inv_of_γ]
-    rw [inv_of_αβ]
-  group
+  grw [expr_β_γ_as_βγ_γ_β hj hk,
+      ← expr_γ_βγ_as_βγ_γ hk hjk,
+      inv_of_αβ]
+  simp
 
 /- Pass between ⁅α,βγ⁆ and ⁅αβ,γ⁆ forms (specializes `Interchange` to the case `u=1`). -/
 theorem InterchangeTrans {i j k : ℕ} (hi : i ≤ α.height) (hj : j ≤ β.height) (hk : k ≤ γ.height) :
@@ -656,11 +635,6 @@ theorem expand_αβγ_as_αβ_γ_αβ_γ :
 
 /-! ### Commutators of αβγ with other roots -/
 
-theorem mul_assoc_α : ∀ {i : ℕ} (hi : i ≤ α.height) (t : R) (b c),
-  b * {α, i, t} * c = b * ({α, i, t} * c) := by
-  intro i hi t b c
-  rw [mul_assoc _ {α, i, t}]
-
 /- α and αβγ commute. -/
 /- NS: One should be able to prove this quite simply:  simple proof: we know αβγ is expressible as a product of αβ's and γ's (expand_αβγ_as_αβ_γ_αβ_γ), and we know that α's
    commute with αβ's (expr_α_αβ_as_αβ_α) and γ's (expr_α_γ_as_γ_α) -/
@@ -670,18 +644,11 @@ theorem comm_of_α_αβγ : trivial_commutator_of_root_pair R pres_mk_of α αβ
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose αβ.height γ.height j hj
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂]
-  mul_assoc_l
-  rw [expr_α_αβ_as_αβ_α hi hj₁]
-  rw [mul_assoc _ {α, i, t}]
-  rw [expr_α_γ_as_γ_α hi hj₂]
-  mul_assoc_l
-  rw [mul_assoc _ {α, i, t}]
-  rw [expr_α_αβ_as_αβ_α hi hj₁]
-  mul_assoc_l
-  rw [mul_assoc _ {α, i, t}]
-  rw [expr_α_γ_as_γ_α hi hj₂]
-  mul_assoc_l
+  grw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂,
+      expr_α_αβ_as_αβ_α hi hj₁,
+      expr_α_γ_as_γ_α hi hj₂,
+      expr_α_αβ_as_αβ_α hi hj₁,
+      expr_α_γ_as_γ_α hi hj₂]
 
 /- γ and αβγ commute. -/
 theorem comm_of_γ_αβγ : trivial_commutator_of_root_pair R pres_mk_of γ αβγ := by
@@ -690,18 +657,11 @@ theorem comm_of_γ_αβγ : trivial_commutator_of_root_pair R pres_mk_of γ αβ
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose α.height βγ.height j hj
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂]
-  mul_assoc_l
-  rw [← expr_α_γ_as_γ_α hj₁ hi]
-  rw [mul_assoc _ {γ, i, t}]
-  rw [expr_γ_βγ_as_βγ_γ hi hj₂]
-  mul_assoc_l
-  rw [mul_assoc _ {γ, i, t}]
-  rw [← expr_α_γ_as_γ_α hj₁ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {γ, i, t}]
-  rw [expr_γ_βγ_as_βγ_γ hi hj₂]
-  mul_assoc_l
+  grw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂,
+    ← expr_α_γ_as_γ_α hj₁ hi,
+    expr_γ_βγ_as_βγ_γ hi hj₂,
+    ← expr_α_γ_as_γ_α hj₁ hi,
+    expr_γ_βγ_as_βγ_γ hi hj₂]
 
 /- β and αβγ commute. -/
 -- the only commutator proof where we have to do something 'interesting'
@@ -711,25 +671,13 @@ theorem comm_of_β_αβγ : trivial_commutator_of_root_pair R pres_mk_of β αβ
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose αβ.height γ.height j hj
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂]
-  mul_assoc_l
-  rw [expr_β_αβ_as_αβ_β hi hj₁]
-  rw [mul_assoc _ {β, i, t}]
-  rw [expr_β_γ_as_γ_βγ_β hi hj₂]
-  mul_assoc_l
-  rw [mul_assoc _ {β, i, t}]
-  rw [expr_β_αβ_as_αβ_β hi hj₁]
-  mul_assoc_l
-  rw [mul_assoc _ {β, i, t}]
-  rw [expr_β_γ_as_βγ_γ_β hi hj₂]
-  rw [mul_assoc _ _ {αβ, j₁, -1}]
-  rw [← expr_αβ_βγ_as_βγ_αβ hj₁ (by simp only [PosRootSys.height, height] at *; omega)]
-  mul_assoc_l
-  rw [mul_assoc _ _ {βγ, i + j₂, t * u}]
-  mul_assoc_l
-  rw [mul_assoc _ {βγ, i + j₂, t * u}]
-  rw [mul_neg]
-  rw [inv_of_βγ]
+  grw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂,
+      expr_β_αβ_as_αβ_β hi hj₁,
+      expr_β_γ_as_γ_βγ_β hi hj₂,
+      expr_β_αβ_as_αβ_β hi hj₁,
+      expr_β_γ_as_βγ_γ_β hi hj₂,
+      ← expr_αβ_βγ_as_βγ_αβ hj₁ (by simp only [PosRootSys.height, height] at *; omega),
+      mul_neg, inv_of_βγ]
   group
 
 /- αβ and αβγ commute. -/
@@ -739,18 +687,11 @@ theorem comm_of_αβ_αβγ : trivial_commutator_of_root_pair R pres_mk_of αβ 
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose α.height βγ.height j hj
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂]
-  mul_assoc_l
-  rw [← expr_α_αβ_as_αβ_α hj₁ hi]
-  rw [mul_assoc _ {αβ, i, t}]
-  rw [expr_αβ_βγ_as_βγ_αβ hi hj₂]
-  mul_assoc_l
-  rw [mul_assoc _ {αβ, i, t}]
-  rw [← expr_α_αβ_as_αβ_α hj₁ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {αβ, i, t}]
-  rw [expr_αβ_βγ_as_βγ_αβ hi hj₂]
-  mul_assoc_l
+  grw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂,
+    ← expr_α_αβ_as_αβ_α hj₁ hi,
+    expr_αβ_βγ_as_βγ_αβ hi hj₂,
+    ← expr_α_αβ_as_αβ_α hj₁ hi,
+    expr_αβ_βγ_as_βγ_αβ hi hj₂]
 
 /- βγ and αβγ commute. -/
 theorem comm_of_βγ_αβγ : trivial_commutator_of_root_pair R pres_mk_of βγ αβγ := by
@@ -759,20 +700,14 @@ theorem comm_of_βγ_αβγ : trivial_commutator_of_root_pair R pres_mk_of βγ 
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose αβ.height γ.height j hj
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂]
-  mul_assoc_l
-  rw [← expr_αβ_βγ_as_βγ_αβ hj₁ hi]
-  rw [mul_assoc _ {βγ, i, t}]
-  rw [← expr_γ_βγ_as_βγ_γ hj₂ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {βγ, i, t}]
-  rw [← expr_αβ_βγ_as_βγ_αβ hj₁ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {βγ, i, t}]
-  rw [← expr_γ_βγ_as_βγ_γ hj₂ hi]
-  mul_assoc_l
+  grw [expand_αβγ_as_αβ_γ_αβ_γ hj₁ hj₂,
+    ← expr_αβ_βγ_as_βγ_αβ hj₁ hi,
+    ← expr_γ_βγ_as_βγ_γ hj₂ hi,
+    ← expr_αβ_βγ_as_βγ_αβ hj₁ hi,
+    ← expr_γ_βγ_as_βγ_γ hj₂ hi]
 
 /- Rewrite α⬝αβγ as αβγ⬝α. -/
+@[group_reassoc]
 theorem expr_α_αβγ_as_αβγ_α :
     ∀ {i j : ℕ} (hi : i ≤ α.height) (hj : j ≤ αβγ.height) (t u : R), commutes({α, i, t}, {αβγ, j, u}) := by
   intro i j hi hj t u
@@ -780,6 +715,7 @@ theorem expr_α_αβγ_as_αβγ_α :
   rw [comm_of_α_αβγ]
 
 /- Rewrite βγ⬝αβγ as αβγ⬝βγ. -/
+@[group_reassoc]
 theorem expr_βγ_αβγ_as_αβγ_βγ :
     ∀ {i j : ℕ} (hi : i ≤ βγ.height) (hj : j ≤ αβγ.height) (t u : R), commutes({βγ, i, t}, {αβγ, j, u}) := by
   intro i j hi hj t u
@@ -793,18 +729,11 @@ theorem mixed_commutes_of_αβγ : trivial_commutator_of_root_pair R pres_mk_of 
   let ⟨ j₁, j₂, ⟨ h_eq, hj₁, hj₂ ⟩ ⟩ := decompose α.height βγ.height j (by trivial)
   simp_rw [h_eq]
   rw [← one_mul u]
-  rw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂]
-  mul_assoc_l
-  rw [← expr_α_αβγ_as_αβγ_α hj₁ hi]
-  rw [mul_assoc _ {αβγ, i, t}]
-  rw [← expr_βγ_αβγ_as_αβγ_βγ hj₂ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {αβγ, i, t}]
-  rw [← expr_α_αβγ_as_αβγ_α hj₁ hi]
-  mul_assoc_l
-  rw [mul_assoc _ {αβγ, i, t}]
-  rw [← expr_βγ_αβγ_as_αβγ_βγ hj₂ hi]
-  mul_assoc_l
+  grw [expand_αβγ_as_α_βγ_α_βγ hj₁ hj₂,
+    ← expr_α_αβγ_as_αβγ_α hj₁ hi,
+    ← expr_βγ_αβγ_as_αβγ_βγ hj₂ hi,
+    ← expr_α_αβγ_as_αβγ_α hj₁ hi,
+    ← expr_βγ_αβγ_as_αβγ_βγ hj₂ hi]
 
 /- Linearity for αβγ. -/
 theorem lin_of_αβγ : lin_of_root R pres_mk_of αβγ := by
@@ -813,20 +742,12 @@ theorem lin_of_αβγ : lin_of_root R pres_mk_of αβγ := by
   have h_eq' : i₁ + i₂ ≤ PosRootSys.height αβγ := by omega
   simp_rw [h_eq]
   nth_rewrite 1 [← mul_one t]
-  rw [expand_αβγ_as_α_βγ_α_βγ hi₁ hi₂]
-  rw [mul_assoc _ _ {αβγ, i₁ + i₂, u}]
-  rw [expr_βγ_αβγ_as_αβγ_βγ hi₂ h_eq']
-  mul_assoc_l
-  rw [mul_assoc _ _ {αβγ, i₁ + i₂, u}]
-  rw [expr_α_αβγ_as_αβγ_α hi₁ h_eq']
-  mul_assoc_l
-  rw [mul_assoc _ _ {αβγ, i₁ + i₂, u}]
-  rw [expr_βγ_αβγ_as_αβγ_βγ hi₂ h_eq']
-  mul_assoc_l
+  grw [expand_αβγ_as_α_βγ_α_βγ hi₁ hi₂,
+    expr_βγ_αβγ_as_αβγ_βγ hi₂ h_eq',
+    expr_α_αβγ_as_αβγ_α hi₁ h_eq',
+    expr_βγ_αβγ_as_αβγ_βγ hi₂ h_eq']
   nth_rewrite 1 [← mul_one u]
-  rw [expand_αβγ_as_α_βγ_α_βγ hi₁ hi₂]
-  mul_assoc_l
-  rw [lin_of_α]
+  grw [expand_αβγ_as_α_βγ_α_βγ hi₁ hi₂, lin_of_α]
   nth_rewrite 1 [inv_of_βγ]
   group
   rw [mul_assoc _ {α, i₁, -u}]
