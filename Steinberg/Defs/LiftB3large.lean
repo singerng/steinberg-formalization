c784aee2e4d8712f89518494c451b8b5bae35390
Eric Wang <ericwang2004@gmail.com>
2025-01-22 17:59:53 -0500

    defined B3LargePosRoot

--- Steinberg/Defs/LiftB3large.lean
+++ Steinberg/Defs/LiftB3large.lean
@@ -26,6 +26,34 @@ namespace Steinberg
 
 open Steinberg.Macro
 
+variable {R : Type TR} [Ring R]
+
+/-! ### Defining the B3 large positive root system -/
+
+inductive B3LargePosRoot
+  | α | β | ψ | αβ | βψ | β2ψ | αβψ | αβ2ψ | α2β2ψ deriving Fintype, DecidableEq
+
+namespace B3LargePosRoot
+
+@[reducible]
+def height : B3LargePosRoot → Nat
+  | α | β | ψ => 1
+  | αβ | βψ => 2
+  | β2ψ | αβψ => 3
+  | αβ2ψ => 4
+  | α2β2ψ => 5
+
+def toString : B3LargePosRoot → String
+  | α => "α"
+  | β => "β"
+  | ψ => "ψ"
+  | αβ => "α+β"
+  | βψ => "β+ψ"
+  | β2ψ => "β+2ψ"
+  | αβψ => "α+β+ψ"
+  | αβ2ψ => "α+β+2ψ"
+  | α2β2ψ => "α+2β+2ψ"
+
 
 /-
 In-subgroup relations
