6ebab57f89766b41cbe03f0b07c0d05c7551568c
Eric Wang <ericwang2004@gmail.com>
2025-01-22 17:45:49 -0500

    LiftB3large

---
+++ Steinberg/Defs/LiftB3large.lean
@@ -0,0 +1,27 @@
+import Mathlib.Data.Finset.Defs
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Order.Interval.Finset.Defs
+import Mathlib.Algebra.Ring.Defs
+
+import Mathlib.Tactic.Group
+import Mathlib.Tactic.FinCases
+import Mathlib.Tactic.DeriveFintype
+
+import Mathlib.GroupTheory.Commutator.Basic
+import Mathlib.GroupTheory.PresentedGroup
+
+import Steinberg.Defs.Root
+import Steinberg.Defs.Chevalley
+import Steinberg.Defs.Deg
+import Steinberg.Defs.Commutator
+import Steinberg.Defs.WeakChevalley
+import Steinberg.Defs.ReflDeg
+
+import Steinberg.Macro.Group
+
+import Steinberg.Upstream.FreeGroup
+import Steinberg.Upstream.PresentedGroup
+
+namespace Steinberg
+
+open Steinberg.Macro
